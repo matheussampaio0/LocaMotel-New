c8afe91ef08b6054965727f42230c1e853e9ef64
matheussampaio0 <47857704+Praz3r@users.noreply.github.com>
2019-12-23 11:49:57 -0300

    Initial commit

---
+++ database/migrations/2019_12_05_181924_create_funcionarios_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 150);
+            $table->string('celular', 15);
+            $table->string('celular2', 15);
+            $table->string('cpf', 16);
+            $table->string('rg', 20);
+            $table->string('rua');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('numerocasa');
+            $table->string('cep');
+            $table->date('datanasc');
+            $table->boolean('acesso');
+            $table->boolean('isAdmin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
