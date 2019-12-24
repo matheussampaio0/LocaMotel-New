888a2854895d994647500b31703cf24629e19f2e
matheussampaio0 <47857704+Praz3r@users.noreply.github.com>
2019-12-23 22:00:23 -0300

    Hot Fixes

--- resources/views/layouts/painel.blade.php
+++ resources/views/layouts/painel.blade.php
@@ -189,6 +189,19 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" 
+              href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">>
+              <i class="nav-icon far fa-circle text-danger"></i>
+              <p class="text">Sair</p>
+              </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+              </form>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
