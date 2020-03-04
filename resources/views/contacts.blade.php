1558e1dbec2b0cb86edeb9d8d5d1d3f611022a8b
Expedito Matheus Sampaio <47857704+matheussampaio0@users.noreply.github.com>
2020-03-04 19:29:35 -0300

    + implementação da página de contatos (1/2)

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.painel')
+
+@section('title') Contatos @endsection
+
+@section('content')
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,22 @@ Route::get('/', function () {
     return view('homepage');
 });
 
-// Faz com que a rota 'register' seja acessada somente se autenticado
-
-
 Auth::routes();
 
+// Rotas do Painel
 Route::group(['prefix' => 'Painel', 'prefix' => 'painel'], function () {
+    
+    // Exibe o painel
     Route::get('/', 'HomeController@index')->name('painel');
+
+    // Exibe os funcionários e as funções UPDATE, ADD e DELETE
     Route::get('/controlefuncionarios', 'FuncionarioController@index')->name('controlfuncionarios');
     Route::put('/controlefuncionarios/update/{id}', 'FuncionarioController@update')->name('controlfuncionariosupdate');
     Route::get('/controlefuncionarios/addfuncionario', 'FuncionarioController@create')->name('controlfuncionarioadd');
     Route::post('/controlefuncionarios/addfuncionario/senddata', 'FuncionarioController@store')->name('controlfuncionarioadddata');
     Route::get('/controlefuncionarios/deletefuncionario/{id}', 'FuncionarioController@show')->name('controlfuncionariodel');
     Route::delete('/controlefuncionarios/deletefuncionario/{id}/deletar', 'FuncionarioController@destroy')->name('controlfuncionariodelete');
+
+    // Exibe o contato dos Funcionários
+    Route::get('/contatos', 'HomeController@contact')->name('contact');
 });
